ad3f2a529685dd4b75e5ac8b0c67bd4d50cadc3f
GraceNjonge <88135535+GraceNjonge@users.noreply.github.com>
2026-03-25 19:21:11 +0300

    Update intro text and role in intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,8 +9,8 @@ const Intro = () => {
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Grace</span> <br />Website Designer</span>
-                <p className="introPara">I am a skilled web designer with experience in creating<br />visually appealing and user friendly websites.</p>
+                <span className="introText">I'm <span className="introName">Grace</span> <br />Front End  Developer</span>
+                <p className="introPara">I am a skilled Front End designer with experience in creating<br />visually appealing and user friendly websites.</p>
                 <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/> Hire Me</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg" />
